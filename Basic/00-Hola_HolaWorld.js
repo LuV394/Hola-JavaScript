77775640c35ff495776c63ff1c6c3871f159b4d1
Luis Vega <l2vega@live.com>
2024-11-24 14:39:52 -0600

    first commit

---
+++ Basic/00-Hola_HolaWorld.js
@@ -0,0 +1,16 @@
+//Este es un comentario simple
+
+/*
+Este es un comentario
+ en varias lineas
+ por dar un ejemplo
+ */
+
+
+ console.log("¡Hola, JavaScript!");
+ console.log('¡Hola, JavaScript!'); 
+ onsole.log('¡Hola, JavaScript!');
+
+ console.log('5');
+ console.log(5);
+  
