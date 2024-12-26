38af5220eb432512c73df29c128db6384465abd5
Luis Vega <l2vega@live.com>
2024-12-26 11:55:32 -0600

    Ejercicios incluidos de la parte 5-6 de la practica del curso de JS

--- Basic/05-operators-excercises.js
+++ Basic/05-operators-excercises.js
@@ -42,10 +42,24 @@ console.log(a>=b);
 
 // 6. Utiliza el operador lógico or
 
+console.log(a>b|| a>=5);
+console.log(a<b || a>=7);
+
+
+
 // 7. Combina ambos operadores lógicos
+
+console.log(a>b && a>3 || b<50);
+
 
 // 8. Añade alguna negación
 
+console.log(!a>=a);
+
+
 // 9. Utiliza el operador ternario
+
+const isProved = !false
+isProved ? console.log("El estuduante aprobó") : console.log("No está reprobado")
 
 // 10. Combina operadores aritméticos, de comparáción y lógicas
---
+++ Basic/06-String.js
@@ -0,0 +1,30 @@
+//String
+let myName = "Luis";
+
+//Concatenacion
+let greeting = "Hola, " + myName + "!";
+
+console.log(greeting);
+
+//longitud
+console.log(greeting.length);
+
+//Accesos a caracteres 
+console.log(greeting[0]);
+console.log(greeting[1]);
+console.log(greeting[6]);
+console.log(greeting[7]);
+console.log(greeting[10]);
+
+//Metodos comunes
+console.log(greeting.toUpperCase());
+console.log(greeting.toLowerCase());
+console.log(greeting.indexOf("Luis"));
+console.log(greeting.indexOf("Vega"));
+console.log(greeting.includes("Luis"));
+console.log(greeting.includes("Vega"));
+console.log(greeting.slice(6,10));
+console.log(greeting.replace("Luis","Vega"));
+
+//templates literals (Plantillas literales)
+
